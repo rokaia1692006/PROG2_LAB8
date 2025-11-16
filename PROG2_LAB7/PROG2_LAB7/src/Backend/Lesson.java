b262e6a2e10bb1824dae9d2921b08e5ef19b728c
rokaia1692006 <rokaia.moh.a@gmail.com>
2025-11-16 04:13:49 +0200

    CLASSES TRAIL

---
+++ PROG2_LAB7/PROG2_LAB7/src/Backend/Lesson.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author it
+ */
+public class Lesson {
+    private String lessonID;
+    private String title;
+    private String content;
+    private ArrayList<String> resources;
+
+    public Lesson(String title, String content, ArrayList resources) {
+        this.lessonID  = Generate.lessonId();
+        this.title = title;
+        this.content = content;
+        this.resources = resources;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public ArrayList<String> getResources() {
+        return resources;
+    }
+
+    public void setResources(ArrayList<String> resources) {
+        this.resources = resources;
+    }
+
+   
+    
+    
+}
