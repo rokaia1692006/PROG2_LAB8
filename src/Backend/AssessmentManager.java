d9dbf9c1e7c8c81ada7923bdd160f3d3a682332b
kithsope <malakhassanabdelhamid@gmail.com>
2025-11-22 15:17:04 +0200

    NSEIT A3ML SAVE YA RB B2A

--- src/Backend/AssessmentManager.java
+++ src/Backend/AssessmentManager.java
@@ -4,10 +4,24 @@
  */
 package Backend;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author malak
  */
 public class AssessmentManager {
     
+    public Quiz createQuiz(ArrayList<Question> questions, int passScore){
+        if(questions==null)
+            throw new IllegalArgumentException("No questions entered.");
+        for(int i=0;i<questions.size();i++){
+            if(questions.get(i)==null||questions.get(i).getQuestion().isEmpty())
+                throw new IllegalArgumentException("Question can't be blank.");
+        }
+        if(passScore==0)
+            throw new IllegalArgumentException("No passing score entered.");
+        Quiz newQuiz=new Quiz(questions, passScore);
+        return newQuiz;
+    }
 }
