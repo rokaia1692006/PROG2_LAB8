754914d7dcfc135094b562a1716fbf1405688319
kithsope <malakhassanabdelhamid@gmail.com>
2025-11-24 02:34:35 +0200

    el mfrood el quiz yban bs idk hn3rf lmma n test

--- src/Frontend/LessonVieww.java
+++ src/Frontend/LessonVieww.java
@@ -111,7 +111,7 @@ public class LessonVieww extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        QuizPage page = new QuizPage(MainFrame,true,l.getQuiz());
+        QuizPage page = new QuizPage((java.awt.Frame) this.getParent(),true,l.getQuiz());
         page.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
